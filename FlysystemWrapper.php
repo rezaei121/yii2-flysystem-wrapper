6c8cef7ad0f45724b2f1e9fe998d1a1d2ce93513
حسین عبایی <hossein142001@gmail.com>
2018-12-25 08:20:00 +0330

    Update FlysystemWrapper.php

--- FlysystemWrapper.php
+++ FlysystemWrapper.php
@@ -134,7 +134,7 @@ class FlysystemWrapper extends \yii\base\Widget
             }
 
             $fmAlais = 'fm_' . $i++;
-            $fileModel->innerJoin([$fmAlais => FileMetadata::tableName()], "f.id={$fmAlais}.file_id AND {$fmAlais}.metadata=:meta_param AND {$fmAlais}.value=:meta_value", ['meta_param' => $meta, 'meta_value' => $value]);
+            $fileModel->innerJoin([$fmAlais => FileMetadata::tableName()], "f.id={$fmAlais}.file_id AND {$fmAlais}.metadata=$meta AND {$fmAlais}.value=$value");
         }
         $fileModel->andWhere(['f.deleted_time' => null]);
 
